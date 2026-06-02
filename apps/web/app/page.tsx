261a073b5cf23a36b86e26f37333946249a053fd
mdriadali <riayedislamrishan@gmail.com>
2026-06-02 16:57:08 +0600

    fix: import prismaclient and env

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,13 @@
-import {prismaClient} from 'db/client'
+import {prismaClient} from '@repo/db'
+import { env } from '@repo/env';
 export default async function Home() {
+  console.log("WEB DATABASE_URL =", env.DATABASE_URL);
+  const envs=env.TESTENV
   const users=await prismaClient.user.findMany()
   return (
-    <div>{JSON.stringify(users)}</div>
+    <div>
+      <h1>envs{envs}</h1>
+      {JSON.stringify(users)||"hellow"}
+    </div>
   );
 }
