6d1ad876a746e7a15497482ca7c2da8381ca3b0a
mdriadali <riayedislamrishan@gmail.com>
2026-06-02 17:04:25 +0600

    feat (http): test init index.ts

--- apps/http-server/index.ts
+++ apps/http-server/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { prismaClient } from "@repo/db";
+import { env } from "@repo/env";
+
+console.log("http DATABASE_URL =", env.DATABASE_URL);
+console.log("http TESTENV =", env.TESTENV);
+
+async function test() {
+  try {
+    const users = await prismaClient.user.findMany();
+    console.log("Users fetched successfully:", users);
+  } catch (error) {
+    console.error("Failed to fetch users:", error);
+  }
+}
+
+test();
