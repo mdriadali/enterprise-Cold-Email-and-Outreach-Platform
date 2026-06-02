d92482cf97949f72308ded1da1597bb12bc836e9
mdriadali <riayedislamrishan@gmail.com>
2026-06-02 16:54:21 +0600

    fix: connectionString

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,8 +1,9 @@
+import { env } from "@repo/env";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: env.DATABASE_URL
 });
 
 export const prismaClient = new PrismaClient({
