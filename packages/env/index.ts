af76448f43ede0f1bfcf5b34a4fb6a7c06880cd4
mdriadali <riayedislamrishan@gmail.com>
2026-06-02 16:53:24 +0600

    fix: env export

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,14 +1,18 @@
-import "dotenv/config"
+import path from "path";
+import dotenv from "dotenv";
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
 
-import { createEnv } from "@t3-oss/env-core"
-import { z } from "zod"
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const env = createEnv({
+
   server: {
     DATABASE_URL: z.string().url(),
+    TESTENV: z.string(),
+    // NEXT_NEW_ENV: z.string(), 
   },
-
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
-})
+  
+  runtimeEnv: process.env, 
+});
