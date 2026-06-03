bbd134acdc7e53ca68a1b2b935b3877f175f2c94
mdriadali <riayedislamrishan@gmail.com>
2026-06-04 02:54:02 +0600

    remove: envs in web index.ts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,11 +2,9 @@ import {prismaClient} from '@repo/db'
 import { env } from '@repo/env';
 export default async function Home() {
   console.log("WEB DATABASE_URL =", env.DATABASE_URL);
-  const envs=env.TESTENV
   const users=await prismaClient.user.findMany()
   return (
     <div>
-      <h1>envs{envs}</h1>
       {JSON.stringify(users)||"hellow"}
     </div>
   );
