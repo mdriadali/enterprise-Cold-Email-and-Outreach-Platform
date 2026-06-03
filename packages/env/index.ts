6e81dea7cc3157920ec357f5a2c0833bc3934abf
mdriadali <riayedislamrishan@gmail.com>
2026-06-04 02:50:11 +0600

    fix: add emptyStringAsUndefined and skipValidation in env pakage index

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,18 +1,14 @@
-import path from "path";
-import dotenv from "dotenv";
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const env = createEnv({
-
   server: {
     DATABASE_URL: z.string().url(),
-    TESTENV: z.string(),
-    // NEXT_NEW_ENV: z.string(), 
+    PORT: z.string(),
   },
-  
-  runtimeEnv: process.env, 
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
