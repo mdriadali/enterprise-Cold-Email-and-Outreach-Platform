009833011e4d120bea73195ca6e058aec729c53e
mdriadali <riayedislamrishan@gmail.com>
2026-06-04 02:56:46 +0600

    feat: make simple express server

--- apps/http-server/index.ts
+++ apps/http-server/index.ts
@@ -1,16 +1,10 @@
-import { prismaClient } from "@repo/db";
 import { env } from "@repo/env";
-
-console.log("http DATABASE_URL =", env.DATABASE_URL);
-console.log("http TESTENV =", env.TESTENV);
-
-async function test() {
-  try {
-    const users = await prismaClient.user.findMany();
-    console.log("Users fetched successfully:", users);
-  } catch (error) {
-    console.error("Failed to fetch users:", error);
-  }
-}
-
-test();
+import Express  from "express";  
+const app = Express();
+const port=env.PORT
+app.get('/help',()=>{
+  console.log('help');
+})
+app.listen(port,()=>{
+  console.log("port:",port)
+})
