f24d56889b2d78992e94e6355e02122458ee84a6
mdriadali <riayedislamrishan@gmail.com>
2026-06-05 02:23:15 +0600

    fix: add pool

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,10 +1,13 @@
 import { env } from "@repo/env";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({
-  connectionString: env.DATABASE_URL
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
 
 export const prismaClient = new PrismaClient({
   adapter,
