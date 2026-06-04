cd513bc01bd2109c12cec91c15c862266ec89756
mdriadali <riayedislamrishan@gmail.com>
2026-06-05 03:07:36 +0600

    fix: env change port to http port

--- apps/http-server/index.ts
+++ apps/http-server/index.ts
@@ -1,7 +1,7 @@
 import { env } from "@repo/env";
 import Express  from "express";  
 const app = Express();
-const port=env.PORT
+const port=env.HTTP_PORT
 app.get('/help',()=>{
   console.log(' end point hit help');
 })
