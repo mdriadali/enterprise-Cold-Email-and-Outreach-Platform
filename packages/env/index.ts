4bb3d06f89619db3e6bb22034e34d94b75df651a
mdriadali <riayedislamrishan@gmail.com>
2026-06-08 22:43:24 +0600

    feat: add JWT_ACCESS_SECRET and JWT_REFRESH_SECRET in env package index file

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -7,6 +7,8 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     HTTP_PORT: z.string(),
+    JWT_ACCESS_SECRET: z.string(),
+    JWT_REFRESH_SECRET: z.string()
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
