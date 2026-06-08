ae9fd529a2888e54d13db3a68ffef639f8aef5a0
mdriadali <riayedislamrishan@gmail.com>
2026-06-08 23:08:22 +0600

    feat (http-server): User  custom errors

---
+++ apps/http-server/src/domain/user/UserError.ts
@@ -0,0 +1,60 @@
+
+
+// Name All errors
+
+export class UserNameRequiredError extends Error {
+    constructor() {
+        super("Name is required or invalid")
+    }
+}
+export class UserNameMinError extends Error {
+    constructor() {
+        super("Name must be at least 2 characters long")
+    }
+}
+export class UserNameMaxError extends Error {
+    constructor() {
+        super("Name must not exceed 20 characters")
+    }
+}
+
+
+//Email all errors
+export class UserEmailRequiredError extends Error {
+    constructor() {
+        super("Email is required")
+    }
+}
+export class UserEmailInvalidError extends Error {
+    constructor() {
+        super("Email is invalid")
+    }
+}
+
+//Password all errors
+export class UserPasswordRequiredError extends Error {
+    constructor() {
+        super("password is required")
+    }
+}
+export class UserPasswordMinError extends Error {
+    constructor() {
+        super("password must be at least 8 characters long")
+    }
+}
+export class UserPasswordMaxError extends Error {
+    constructor() {
+        super("password must not exceed 16 characters")
+    }
+}
+export class UserExistError extends Error {
+    constructor() {
+        super("user already exist")
+    }
+}
+
+
+
+
+
+
