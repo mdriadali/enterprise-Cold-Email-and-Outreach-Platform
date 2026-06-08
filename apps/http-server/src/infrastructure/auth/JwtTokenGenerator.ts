be2046b9890dd131d6a8ccb77c70eaa20f9535e3
mdriadali <riayedislamrishan@gmail.com>
2026-06-09 02:16:31 +0600

    fix (http-server): env import in JwtTokenGenerator

--- apps/http-server/src/infrastructure/auth/JwtTokenGenerator.ts
+++ apps/http-server/src/infrastructure/auth/JwtTokenGenerator.ts
@@ -1,19 +1,19 @@
 import jwt from "jsonwebtoken";
 import type { IJwtTokenProvider } from "../../application/ports/auth/IJwtTokenProvider-ports"
-import { env } from "@repo/env";
+import { serverEnv } from "@repo/env/server-env";
 
 export class JwtTokenGenerator implements IJwtTokenProvider {
     async generateAccessToken(UserId: string): Promise<string> {
         const acessToken: string = jwt.sign({
             data: UserId
-        }, env.JWT_ACCESS_SECRET, { expiresIn: '15m' });
+        }, serverEnv.JWT_ACCESS_SECRET, { expiresIn: '15m' });
 
         return acessToken
     }
     async generateRefreshToken(UserId: string): Promise<string> {
         const refreshToken: string = jwt.sign({
             data: UserId
-        }, env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
+        }, serverEnv.JWT_REFRESH_SECRET, { expiresIn: '30d' });
 
         return refreshToken
     }
