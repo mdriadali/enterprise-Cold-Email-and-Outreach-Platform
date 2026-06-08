973440fc082cfd1ad923ca0a99c14a48bc2a4b79
mdriadali <riayedislamrishan@gmail.com>
2026-06-09 02:14:34 +0600

    fix (web): import sharedEnv in page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,9 @@
 export const dynamic = "force-dynamic";
 import {prismaClient} from '@repo/db'
-import { env } from '@repo/env';
+import { sharedEnv } from '@repo/env/shared-env';
+
 export default async function Home() {
-  console.log("WEB DATABASE_URL =", env.DATABASE_URL);
+  console.log("WEB DATABASE_URL =", sharedEnv.DATABASE_URL);
   const users=await prismaClient.user.findMany()
   return (
     <div>
