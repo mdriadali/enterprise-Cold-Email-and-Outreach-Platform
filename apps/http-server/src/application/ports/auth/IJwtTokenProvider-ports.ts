0deabde2838eb8f1fc28d4111352ade47463e931
mdriadali <riayedislamrishan@gmail.com>
2026-06-10 02:18:26 +0600

    feat (http-server): add validateAccessToken method IJwtTokenProvider

--- apps/http-server/src/application/ports/auth/IJwtTokenProvider-ports.ts
+++ apps/http-server/src/application/ports/auth/IJwtTokenProvider-ports.ts
@@ -1,4 +1,7 @@
+import type { TokenPayload } from "@repo/types"
+
 export interface IJwtTokenProvider{
     generateAccessToken(UserId:string):Promise<string>
     generateRefreshToken(UserId:string):Promise<string>
+    validateAccessToken(token:string):Promise<TokenPayload>
 }
