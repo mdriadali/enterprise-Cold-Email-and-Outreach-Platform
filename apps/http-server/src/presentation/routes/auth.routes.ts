a671d01eacbed0ab88874c5b1fdf6deba1a4e8d7
mdriadali <riayedislamrishan@gmail.com>
2026-06-09 23:23:17 +0600

    feat: add login route and add usecase dependenci in auth route

--- apps/http-server/src/presentation/routes/auth.routes.ts
+++ apps/http-server/src/presentation/routes/auth.routes.ts
@@ -1,22 +1,34 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
-import { RegisterUseCase } from "../../application/use-cases/auth/RegisterUseCase";
 import { BcryptPasswordHasher } from "../../infrastructure/auth/BcryptPasswordHasher";
 import { PrismaUserRepository } from "../../infrastructure/repositories/PrismaUserRepository";
 import { JwtTokenGenerator } from "../../infrastructure/auth/JwtTokenGenerator";
+import { RegisterUserUseCase } from "../../application/use-cases/auth/RegisterUser-UseCase";
+import { LoginUserUseCase } from "../../application/use-cases/auth/LoginUser-UseCase";
 
 const authRouter = Router();
 const bcryptPasswordHasher = new BcryptPasswordHasher
 const prismaUserRepository = new PrismaUserRepository
-const jwtTokenGenerator=new JwtTokenGenerator
+const jwtTokenGenerator = new JwtTokenGenerator
+
 const registerUseCase =
-    new RegisterUseCase(
+    new RegisterUserUseCase(
         bcryptPasswordHasher,
         prismaUserRepository,
         jwtTokenGenerator
     );
-const authController = new AuthController(registerUseCase);
+
+const loginUserUseCase = new LoginUserUseCase(
+    prismaUserRepository,
+    bcryptPasswordHasher,
+    jwtTokenGenerator
+)
+
+
+
+const authController = new AuthController(registerUseCase, loginUserUseCase);
 
 authRouter.post("/register", authController.register);
+authRouter.post("/login", authController.login)
 
 export default authRouter;
