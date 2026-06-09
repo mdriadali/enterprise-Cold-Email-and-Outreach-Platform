2d3117f20f7cf53c8dc46ebcb9bb78818ac5c7fc
mdriadali <riayedislamrishan@gmail.com>
2026-06-09 23:27:01 +0600

    feat (http-server): add login controller and use case constractor in auth controller

--- apps/http-server/src/presentation/controllers/AuthController.ts
+++ apps/http-server/src/presentation/controllers/AuthController.ts
@@ -1,27 +1,58 @@
 import type { Request, Response } from "express";
-import { RegisterUseCase } from "../../application/use-cases/auth/RegisterUseCase";
-import type { RegisterUserInput } from "@repo/types";
+
+import type { LoginUserInput, RegisterUserInput } from "@repo/types";
+import type { RegisterUserUseCase } from "../../application/use-cases/auth/RegisterUser-UseCase";
+import type { LoginUserUseCase } from "../../application/use-cases/auth/LoginUser-UseCase";
 
 
 export class AuthController {
   constructor(
-    private readonly registerUseCase: RegisterUseCase
+    private readonly registerUseCase: RegisterUserUseCase,
+    private readonly loginUserUseCase: LoginUserUseCase
   ) { }
 
   register = async (req: Request, res: Response) => {
-    const data: RegisterUserInput = req.body;
-    const result = await this.registerUseCase.execute(data);
+    try {
+      const data: RegisterUserInput = req.body;
+      const result = await this.registerUseCase.execute(data);
 
-    res.cookie(
-      "accessToken",
-      result.accessToken
-    );
+      res.cookie(
+        "accessToken",
+        result.accessToken
+      );
 
-    res.cookie(
-      "refreshToken",
-      result.refreshToken
-    );
+      res.cookie(
+        "refreshToken",
+        result.refreshToken
+      );
 
-    return res.json({ sucess: true });
+      return res.status(200).json({ sucess: true });
+    } catch (error) {
+      return res.status(500).json({
+        message: error instanceof Error ? error.message : "somthing went wrong"
+      });
+    }
   };
+
+  login = async (req: Request, res: Response) => {
+    try {
+      console.log("[LOGIN] Request received");
+      const loginData: LoginUserInput = req.body
+      const result = await this.loginUserUseCase.execute(loginData)
+      res.cookie(
+        "accessToken",
+        result.accessToken
+      )
+      res.cookie(
+        "refreshToken",
+        result.refreshToken
+      )
+
+      return res.status(200).json({ sucess: true })
+    } catch (error: unknown) {
+      return res.status(500).json({
+        message: error instanceof Error ? error.message : "somthing went wrong"
+      });
+    }
+  }
 }
