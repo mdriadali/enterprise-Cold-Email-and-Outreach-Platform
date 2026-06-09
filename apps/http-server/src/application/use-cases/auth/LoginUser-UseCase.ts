583b139aaa8201f7df93130776dde2b4424a558d
mdriadali <riayedislamrishan@gmail.com>
2026-06-09 23:38:16 +0600

    feat: add LoginUserUseCase and execute method

---
+++ apps/http-server/src/application/use-cases/auth/LoginUser-UseCase.ts
@@ -0,0 +1,39 @@
+import type { LoginUserInput } from "@repo/types";
+import { LoginUserEntity } from "../../../domain/auth/Login-entity";
+import type { IUserRepository } from "../../ports/repositories/UserRepository-ports";
+import { UserValidator } from "../../../domain/user/UserValidator";
+import type { IPasswordHasher } from "../../ports/auth/IPasswordHasher-ports";
+import { HashValidator } from "../../../domain/auth/HashValidator";
+import type { IJwtTokenProvider } from "../../ports/auth/IJwtTokenProvider-ports";
+
+export class LoginUserUseCase {
+    constructor(
+        private readonly userRepository: IUserRepository,
+        private readonly passwordHasher: IPasswordHasher,
+        private readonly jwtTokenProvider: IJwtTokenProvider
+    ) { }
+
+
+    async execute(LoginData: LoginUserInput) {
+        console.log("[LOGIN] user attmting login:", LoginData.email);
+        new LoginUserEntity(
+            LoginData.email,
+            LoginData.password
+        )
+        console.log("login entity run sucessfully ")
+        const userExist = await this.userRepository.findByEmail(LoginData.email)
+        UserValidator.UserNotExist(userExist)
+        console.log("[Login] user found")
+
+        const isHashMatch = await this.passwordHasher.hashcompare(LoginData.password, userExist!.password)
+        HashValidator.isHashValidate(isHashMatch)
+
+        console.log("[Login] password verified")
+
+        const accessToken = await this.jwtTokenProvider.generateAccessToken(userExist!.id)
+        const refreshToken = await this.jwtTokenProvider.generateRefreshToken(userExist!.id)
+        console.log("[Login]jwt token generated")
+        return { accessToken, refreshToken }
+
+    }
+}
