4aae3f198ec4250cd110a73b68ced368ed84164d
mdriadali <riayedislamrishan@gmail.com>
2026-06-10 02:13:41 +0600

    feat (http-server): implement validateAccessToken in JwtTokenGenerator

--- apps/http-server/src/infrastructure/auth/JwtTokenGenerator.ts
+++ apps/http-server/src/infrastructure/auth/JwtTokenGenerator.ts
@@ -1,20 +1,30 @@
-import jwt from "jsonwebtoken";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import type { IJwtTokenProvider } from "../../application/ports/auth/IJwtTokenProvider-ports"
 import { serverEnv } from "@repo/env/server-env";
+import type { TokenPayload } from "@repo/types";
 
 export class JwtTokenGenerator implements IJwtTokenProvider {
     async generateAccessToken(UserId: string): Promise<string> {
         const acessToken: string = jwt.sign({
-            data: UserId
+            UserId
         }, serverEnv.JWT_ACCESS_SECRET, { expiresIn: '15m' });
 
         return acessToken
     }
     async generateRefreshToken(UserId: string): Promise<string> {
         const refreshToken: string = jwt.sign({
-            data: UserId
+            UserId
         }, serverEnv.JWT_REFRESH_SECRET, { expiresIn: '30d' });
 
         return refreshToken
+    }
+
+    async validateAccessToken(token: string): Promise<TokenPayload> {
+
+        const payload = jwt.verify(
+            token,
+            serverEnv.JWT_ACCESS_SECRET!
+        ) as TokenPayload
+        return payload
     }
 }
