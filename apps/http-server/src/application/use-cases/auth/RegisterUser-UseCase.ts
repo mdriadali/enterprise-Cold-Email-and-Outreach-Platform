c808b818f2dac4e58c84ac833a027626ba9f69eb
mdriadali <riayedislamrishan@gmail.com>
2026-06-09 23:37:39 +0600

    refector (http-server): change class name and file path

--- apps/http-server/src/application/use-cases/auth/RegisterUser-UseCase.ts
+++ apps/http-server/src/application/use-cases/auth/RegisterUser-UseCase.ts
@@ -1,34 +1,34 @@
 import type { RegisterUserInput } from "@repo/types";
-import { User } from "../../../domain/user/User";
 import type { IPasswordHasher } from "../../ports/auth/IPasswordHasher-ports";
 import type { IUserRepository } from "../../ports/repositories/UserRepository-ports";
 import type { IJwtTokenProvider } from "../../ports/auth/IJwtTokenProvider-ports";
 import { UserValidator } from "../../../domain/user/UserValidator";
+import { RegisterUserEntity } from "../../../domain/auth/Register-entity";
 
 
-export class RegisterUseCase {
+export class RegisterUserUseCase {
 
     constructor(
         private readonly passwordHasher: IPasswordHasher,
         private readonly userRepository: IUserRepository,
         private readonly jwtTokenProvider: IJwtTokenProvider
     ) { }
-    async execute(data: RegisterUserInput) {
-        const user = new User(
-            data.name,
-            data.email,
-            data.password,
+    async execute(registerdata: RegisterUserInput) {
+        const registerEntity = new RegisterUserEntity(
+            registerdata.name,
+            registerdata.email,
+            registerdata.password,
         )
 
-        const existUser = await this.userRepository.findByEmail(data.email)
+        const existUser = await this.userRepository.findByEmail(registerdata.email)
 
         UserValidator.userExist(existUser)
 
 
-        const hashpassword = await this.passwordHasher.hash(data.password)
+        const hashpassword = await this.passwordHasher.hash(registerdata.password)
         const createdUser = await this.userRepository.create({
-            name: user.name,
-            email: user.email,
+            name: registerdata.name,
+            email: registerdata.email,
             password: hashpassword
         })
 
