84039561457f8744fdf6cd356903c1b9e2143a66
mdriadali <riayedislamrishan@gmail.com>
2026-06-10 02:17:49 +0600

    feat (http-server): add findById method IUserRepository

--- apps/http-server/src/application/ports/repositories/UserRepository-ports.ts
+++ apps/http-server/src/application/ports/repositories/UserRepository-ports.ts
@@ -3,4 +3,5 @@ import type { CreatedUserData,  RegisterUserInput,  Userdata } from "@repo/types
 export interface IUserRepository {
     create(data: RegisterUserInput): Promise<CreatedUserData>;
     findByEmail(email: string): Promise<Userdata|null>
+    findById(id:string):Promise<Userdata|null>
 }
