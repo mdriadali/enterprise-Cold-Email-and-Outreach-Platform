8f6f7eb062ea305198177b038fcda5683986866c
mdriadali <riayedislamrishan@gmail.com>
2026-06-11 00:37:03 +0600

    feat (http-server): LogoutUserUseCase

---
+++ apps/http-server/src/application/use-cases/auth/Logoutuser-useCase.ts
@@ -0,0 +1,24 @@
+import { AuthValidator } from "../../../domain/auth/AuthValidator";
+import { UnauthorizedError } from "../../../domain/auth/Error";
+import type { IRefreshTokenRepository } from "../../ports/repositories/RefreshTokenRepository-ports";
+
+export class LogoutUserUseCase {
+    constructor(
+        private readonly refreshTokenRepository: IRefreshTokenRepository
+
+    ) { }
+    async execute(userId: string, refreshToken: string) {
+        console.log("[Logout] user trying", userId)
+        const tokenData = await this.refreshTokenRepository.findByToken(refreshToken)
+        AuthValidator.tokenDataValidator(tokenData)
+
+
+        if (userId !== tokenData?.userId) {
+            throw new UnauthorizedError()
+        }
+
+        await this.refreshTokenRepository.deleteByToken(refreshToken)
+        console.log("[Logout] user logout sucessfully", userId)
+        return true
+    }
+}
