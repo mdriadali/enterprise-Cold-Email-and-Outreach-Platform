c09529c5b9116960f7d67084ed31f39e565575fa
mdriadali <riayedislamrishan@gmail.com>
2026-06-11 00:36:02 +0600

    fix (http-serevr): generateRefreshToken types and return expiresAt also in JwtTokenGenerator implementation class

--- apps/http-server/src/infrastructure/auth/JwtTokenGenerator.ts
+++ apps/http-server/src/infrastructure/auth/JwtTokenGenerator.ts
@@ -1,7 +1,7 @@
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import type { IJwtTokenProvider } from "../../application/ports/auth/IJwtTokenProvider-ports"
 import { serverEnv } from "@repo/env/server-env";
-import type { TokenPayload } from "@repo/types";
+import type { generateRefreshTokenTypes, TokenPayload } from "@repo/types";
 
 export class JwtTokenGenerator implements IJwtTokenProvider {
     async generateAccessToken(UserId: string): Promise<string> {
@@ -11,16 +11,21 @@ export class JwtTokenGenerator implements IJwtTokenProvider {
 
         return acessToken
     }
-    async generateRefreshToken(UserId: string): Promise<string> {
+    async generateRefreshToken(UserId: string): Promise<generateRefreshTokenTypes> {
+        const REFRESH_TOKEN_DAYS = 30;
+
+        const expiresAt = new Date(
+            Date.now() + REFRESH_TOKEN_DAYS * 24 * 60 * 60 * 1000
+        )
         const refreshToken: string = jwt.sign({
             UserId
         }, serverEnv.JWT_REFRESH_SECRET, { expiresIn: '30d' });
 
-        return refreshToken
+        return { token: refreshToken, expiresAt: expiresAt }
     }
 
     async validateAccessToken(token: string): Promise<TokenPayload> {
-
+        console.log("trying to validate access token")
         const payload = jwt.verify(
             token,
             serverEnv.JWT_ACCESS_SECRET!
