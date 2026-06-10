2e2fdd460a7735579d8e990f086c3d8a6e594319
mdriadali <riayedislamrishan@gmail.com>
2026-06-11 00:28:43 +0600

    fix (http-serever): generateRefreshToken types in  IJwtTokenProvider ports

--- apps/http-server/src/application/ports/auth/IJwtTokenProvider-ports.ts
+++ apps/http-server/src/application/ports/auth/IJwtTokenProvider-ports.ts
@@ -1,7 +1,7 @@
-import type { TokenPayload } from "@repo/types"
+import type { generateRefreshTokenTypes, TokenPayload } from "@repo/types"
 
 export interface IJwtTokenProvider{
     generateAccessToken(UserId:string):Promise<string>
-    generateRefreshToken(UserId:string):Promise<string>
+    generateRefreshToken(UserId:string):Promise< generateRefreshTokenTypes>
     validateAccessToken(token:string):Promise<TokenPayload>
 }
