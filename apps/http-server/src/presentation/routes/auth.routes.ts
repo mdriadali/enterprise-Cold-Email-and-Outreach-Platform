74009337ec2e7477ac314fbac33d37741b6320fc
mdriadali <riayedislamrishan@gmail.com>
2026-06-11 00:23:29 +0600

    feat (http-server): add logout route in auth routes

--- apps/http-server/src/presentation/routes/auth.routes.ts
+++ apps/http-server/src/presentation/routes/auth.routes.ts
@@ -5,30 +5,46 @@ import { PrismaUserRepository } from "../../infrastructure/repositories/PrismaUs
 import { JwtTokenGenerator } from "../../infrastructure/auth/JwtTokenGenerator";
 import { RegisterUserUseCase } from "../../application/use-cases/auth/RegisterUser-UseCase";
 import { LoginUserUseCase } from "../../application/use-cases/auth/LoginUser-UseCase";
+import { PrismaRefreshToken } from "../../infrastructure/repositories/PrismaRefreshToken";
+import { AuthMiddleware } from "../middlewares/AuthMiddleware";
+import { LogoutUserUseCase } from "../../application/use-cases/auth/Logoutuser-useCase";
 
 const authRouter = Router();
 const bcryptPasswordHasher = new BcryptPasswordHasher
 const prismaUserRepository = new PrismaUserRepository
 const jwtTokenGenerator = new JwtTokenGenerator
+const prismaRefreshToken = new PrismaRefreshToken
+
 
 const registerUseCase =
     new RegisterUserUseCase(
         bcryptPasswordHasher,
         prismaUserRepository,
-        jwtTokenGenerator
+        jwtTokenGenerator,
+        prismaRefreshToken
     );
 
 const loginUserUseCase = new LoginUserUseCase(
     prismaUserRepository,
     bcryptPasswordHasher,
-    jwtTokenGenerator
+    jwtTokenGenerator,
+    prismaRefreshToken
+)
+const logoutUserUseCase = new LogoutUserUseCase(
+    prismaRefreshToken
+)
+
+const authMiddleware = new AuthMiddleware(
+    jwtTokenGenerator,
+    prismaUserRepository,
 )
 
 
 
-const authController = new AuthController(registerUseCase, loginUserUseCase);
+const authController = new AuthController(registerUseCase, loginUserUseCase, logoutUserUseCase);
 
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login)
+authRouter.post("/logout", authMiddleware.execute.bind(authMiddleware), authController.logout)
 
 export default authRouter;
