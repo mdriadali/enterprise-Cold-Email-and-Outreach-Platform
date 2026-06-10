6627acdc49c075c61e5b8a9c3970006fbcdc1cd1
mdriadali <riayedislamrishan@gmail.com>
2026-06-11 00:33:55 +0600

    feat (http-server): add tokenDataValidator and add error method in  AuthValidator

--- apps/http-server/src/domain/auth/AuthValidator.ts
+++ apps/http-server/src/domain/auth/AuthValidator.ts
@@ -1,15 +1,22 @@
+import type { RefreshtokenData } from "@repo/types"
+import { UnauthorizedError } from "./Error"
 
 
-export class AuthValidator{
-    static isHashValidate(match:boolean){
-        if(match===false){
+export class AuthValidator {
+    static isHashValidate(match: boolean) {
+        if (match === false) {
             throw new Error("Invalid credentials")
         }
     }
 
-    static tokenValidator(token:string){
-        if(!token){
-            throw new Error("Unauthorized")
+    static tokenValidator(token: string) {
+        if (!token) {
+            throw new UnauthorizedError()
+        }
+    }
+    static tokenDataValidator(tokenData: RefreshtokenData | null) {
+        if (!tokenData) {
+            throw new UnauthorizedError()
         }
     }
 }
