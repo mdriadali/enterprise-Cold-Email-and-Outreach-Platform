892bcfaf9abb57bcbffae24d9f2fa38fd6dd5ffd
mdriadali <riayedislamrishan@gmail.com>
2026-06-11 00:18:50 +0600

    feat (types):RegisterUserInput and LoginUserInput add deviceInfo in auth types

--- packages/types/src/auth-types.ts
+++ packages/types/src/auth-types.ts
@@ -4,15 +4,49 @@ export interface RegisterUserInput {
     name: string,
     email: string,
     password: string,
+    deviceInfo?: {
+        deviceName?: string | undefined;
+        ipAddress?: string | undefined;
+        userAgent?: string | undefined;
+    }
 }
 
 export interface LoginUserInput {
     email: string,
     password: string,
+    deviceInfo?: {
+        deviceName?: string | undefined;
+        ipAddress?: string | undefined;
+        userAgent?: string | undefined;
+    }
 }
 
 
 
 export interface TokenPayload extends JwtPayload {
     userId: string;
+}
+export interface generateRefreshTokenTypes {
+    token: string
+    expiresAt: Date
+}
+
+export interface RefreshTokenTypes {
+    token: string
+    userId: string
+    deviceName?: string
+    ipAddress?: string
+    userAgent?: string
+    expiresAt: Date
+}
+
+export interface RefreshtokenData {
+    id: string
+    token: string
+    userId: string
+    deviceName: string|null
+    ipAddress: string|null
+    userAgent: string|null
+    createdAt: Date
+    expiresAt: Date
 }
