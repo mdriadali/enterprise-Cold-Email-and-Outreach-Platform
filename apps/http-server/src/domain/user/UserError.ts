ff9840acb51ef7b42683b7272c6b74ef2cb56473
mdriadali <riayedislamrishan@gmail.com>
2026-06-12 00:53:24 +0600

    fix (http-server): remove status code in usererror file

--- apps/http-server/src/domain/user/UserError.ts
+++ apps/http-server/src/domain/user/UserError.ts
@@ -6,17 +6,17 @@ import { AppError } from "../AppError"
 
 export class UserNameRequiredError extends AppError {
     constructor() {
-        super("Name is required or invalid",400)
+        super("Name is required or invalid")
     }
 }
 export class UserNameMinError extends AppError {
     constructor() {
-        super("Name must be at least 2 characters long",400)
+        super("Name must be at least 2 characters long")
     }
 }
 export class UserNameMaxError extends AppError {
     constructor() {
-        super("Name must not exceed 20 characters",400)
+        super("Name must not exceed 20 characters")
     }
 }
 
@@ -24,39 +24,39 @@ export class UserNameMaxError extends AppError {
 //Email all errors
 export class UserEmailRequiredError extends AppError {
     constructor() {
-        super("Email is required",400)
+        super("Email is required")
     }
 }
 export class UserEmailInvalidError extends AppError {
     constructor() {
-        super("Email is invalid",400)
+        super("Email is invalid")
     }
 }
 
 //Password all errors
 export class UserPasswordRequiredError extends AppError {
     constructor() {
-        super("password is required",400)
+        super("password is required")
     }
 }
 export class UserPasswordMinError extends AppError {
     constructor() {
-        super("password must be at least 8 characters long",400)
+        super("password must be at least 8 characters long")
     }
 }
 export class UserPasswordMaxError extends AppError {
     constructor() {
-        super("password must not exceed 16 characters",400)
+        super("password must not exceed 16 characters")
     }
 }
 export class UserExistError extends AppError {
     constructor() {
-        super("user already exist",400)
+        super("user already exist")
     }
 }
 export class UserNotExistError extends AppError {
     constructor() {
-        super("user not exist",400)
+        super("user not exist")
     }
 }
 
