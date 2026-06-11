d92d2b43befd33cb027e0e30f456da2f873367bc
mdriadali <riayedislamrishan@gmail.com>
2026-06-12 00:57:20 +0600

    feat (http-server): add validateRefreshToken in IJwtTokenProvider interface

--- apps/http-server/src/application/ports/auth/IJwtTokenProvider-ports.ts
+++ apps/http-server/src/application/ports/auth/IJwtTokenProvider-ports.ts
@@ -4,4 +4,5 @@ export interface IJwtTokenProvider{
     generateAccessToken(UserId:string):Promise<string>
     generateRefreshToken(UserId:string):Promise< generateRefreshTokenTypes>
     validateAccessToken(token:string):Promise<TokenPayload>
+    validateRefreshToken(token:string):Promise<TokenPayload>
 }
