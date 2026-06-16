2899cd0d4ee932fb646d7b234607de6fe3ba0259
mdriadali <riayedislamrishan@gmail.com>
2026-06-17 01:06:39 +0600

    feat (http-server): workspace Error file

---
+++ apps/http-server/src/domain/workspace/workspaceError.ts
@@ -0,0 +1,19 @@
+import { AppError } from "../AppError";
+import { workspacerules } from "./workspaceRules";
+
+export class workspaceNameInvlid extends AppError {
+    constructor() {
+        super("Workspace name invalid")
+    }
+}
+
+export class workspaceNameMinError extends AppError {
+    constructor() {
+        super(`Workspace name must be at least ${workspacerules.MIN_NAME} characters long`)
+    }
+}
+export class workspaceNameMaxError extends AppError {
+    constructor() {
+        super(`Name must not exceed ${workspacerules.MAX_NAME} characters`)
+    }
+}
