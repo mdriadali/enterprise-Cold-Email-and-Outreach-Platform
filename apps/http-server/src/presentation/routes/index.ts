63a5c50ca2a534b7581682a5928a12b58fc7269c
mdriadali <riayedislamrishan@gmail.com>
2026-06-17 01:03:11 +0600

    feat (http-server): add workspace route in routes index

--- apps/http-server/src/presentation/routes/index.ts
+++ apps/http-server/src/presentation/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
+import workspaceRouter from "./workspace.routes";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/user",userRouter)
+router.use("/user",userRouter);
+router.use("/workspace",workspaceRouter)
 
 export default router;
