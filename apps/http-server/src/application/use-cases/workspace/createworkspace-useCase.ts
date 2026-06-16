77dddcf16d3a41c8ea0abf9c5010c301c99f78b0
mdriadali <riayedislamrishan@gmail.com>
2026-06-17 01:05:17 +0600

    feat (http-server): CreateWorkspaceUseCase

---
+++ apps/http-server/src/application/use-cases/workspace/createworkspace-useCase.ts
@@ -0,0 +1,26 @@
+import { workspaceValidator } from "../../../domain/workspace/workspaceValidator"
+import type { IWorkspaceMemberRepository } from "../../ports/repositories/WorkspaceMemberRepository-ports"
+import type { IWorkspaceRepository } from "../../ports/repositories/WorkspaceRepository-ports"
+
+export class CreateWorkspaceUseCase {
+    constructor(
+        private readonly workspaceRepository: IWorkspaceRepository,
+        private readonly workspaceMemberRepository: IWorkspaceMemberRepository
+    ) { }
+    async execute(userId: string, name: string) {
+        console.log("[workspace create] trying user", userId)
+        workspaceValidator.validateName(name)
+
+        const newWorkspace = await this.workspaceRepository.create(userId, name)
+        const newWorkspaceMember = await this.workspaceMemberRepository.create({
+            workspaceId: newWorkspace.id,
+            memberId: newWorkspace.ownerId,
+            role: "OWNER"
+        })
+
+        return {
+            workspace: newWorkspace,
+            member: newWorkspaceMember
+        }
+    }
+}
