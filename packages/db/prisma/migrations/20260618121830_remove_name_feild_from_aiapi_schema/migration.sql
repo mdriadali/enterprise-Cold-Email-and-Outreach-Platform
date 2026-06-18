17cf8423bbe0a54489b2e32117cc93136e5e400a
mdriadali <riayedislamrishan@gmail.com>
2026-06-18 19:59:53 +0600

    fix( db): remove name feild in aiapi schema maigration

---
+++ packages/db/prisma/migrations/20260618121830_remove_name_feild_from_aiapi_schema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `AiApi` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "AiApi" DROP COLUMN "name";
