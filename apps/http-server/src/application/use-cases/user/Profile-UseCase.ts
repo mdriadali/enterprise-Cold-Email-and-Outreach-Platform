d67cf99b479852c0e13978ee97941f0077b489ef
mdriadali <riayedislamrishan@gmail.com>
2026-06-18 19:57:42 +0600

    fix (http-server): remove ai provider and api key return in ProfileUseCase

--- apps/http-server/src/application/use-cases/user/Profile-UseCase.ts
+++ apps/http-server/src/application/use-cases/user/Profile-UseCase.ts
@@ -1,5 +1,4 @@
 import { UserValidator } from "../../../domain/user/UserValidator"
-import { maskApiKey } from "../../../utils/maskApiKey"
 import type { IUserRepository } from "../../ports/repositories/UserRepository-ports"
 
 export class ProfileUseCase {
@@ -10,14 +9,11 @@ export class ProfileUseCase {
         console.log("[profile] trying to get data User: ", userId)
         const userData = await this.userRepository.findById(userId)
         UserValidator.UserNotExist(userData)
-        const maskApi = maskApiKey(userData!.apiKey)
         return {
             id: userData?.id,
             name: userData?.name,
             email: userData?.email,
             role: userData?.role,
-            aiProvider: userData?.aiProvider,
-            apiKey: maskApi
         }
     }
 }
