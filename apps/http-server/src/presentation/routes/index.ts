b7b06cbfb907965723e11d82247300cb98f91734
mdriadali <riayedislamrishan@gmail.com>
2026-06-18 19:44:42 +0600

    feat (http-serever): add aiapi route in routes index

--- apps/http-server/src/presentation/routes/index.ts
+++ apps/http-server/src/presentation/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
 import workspaceRouter from "./workspace.routes";
+import aiApiRouter from "./aiapi.routes";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/user",userRouter);
 router.use("/workspace",workspaceRouter)
+router.use("/aiapi",aiApiRouter)
 
 export default router;
