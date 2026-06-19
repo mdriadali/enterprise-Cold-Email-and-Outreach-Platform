da2e40bb9984543bb087aa37a6adb762f7031e85
mdriadali <riayedislamrishan@gmail.com>
2026-06-20 02:11:37 +0600

    feat (http-server): add generationJobRepository depindennci in shaare dependenci file

--- apps/http-server/src/presentation/container/share-dependencies.ts
+++ apps/http-server/src/presentation/container/share-dependencies.ts
@@ -1,6 +1,7 @@
 import { BcryptPasswordHasher } from "../../infrastructure/auth/BcryptPasswordHasher"
 import { JwtTokenGenerator } from "../../infrastructure/auth/JwtTokenGenerator"
 import { PrismaAiApiRepository } from "../../infrastructure/repositories/PrismaAiApiRepository"
+import { PrismaGenerationJobRepository } from "../../infrastructure/repositories/PrismaGenerationJobRepository"
 import { PrismaRefreshToken } from "../../infrastructure/repositories/PrismaRefreshToken"
 import { PrismaUserRepository } from "../../infrastructure/repositories/PrismaUserRepository"
 import { PrismaWorkspace } from "../../infrastructure/repositories/PrismaWorkspace"
@@ -13,3 +14,4 @@ export const prismaRefreshToken = new PrismaRefreshToken
 export const workspaceRepository=new PrismaWorkspace
 export const workspaceMemberRepository=new PrismaWorkspaceMember
 export const prismaAiApiRepository=new PrismaAiApiRepository
+export const generationJobRepository=new PrismaGenerationJobRepository 
