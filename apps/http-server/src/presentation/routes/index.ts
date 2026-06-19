dcb19a883ebc4c3517c0e4b39a62b38f7aed1f5e
mdriadali <riayedislamrishan@gmail.com>
2026-06-20 02:06:46 +0600

    feat (http-server):add generationjob route in route index

--- apps/http-server/src/presentation/routes/index.ts
+++ apps/http-server/src/presentation/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
 import workspaceRouter from "./workspace.routes";
 import aiApiRouter from "./aiapi.routes";
+import generationJobRouter from "./generationJob.routes";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRouter);
 router.use("/user",userRouter);
 router.use("/workspace",workspaceRouter)
 router.use("/aiapi",aiApiRouter)
+router.use("/generationjob",generationJobRouter)
 
 export default router;
