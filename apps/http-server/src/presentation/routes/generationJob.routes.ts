c038ffc7b1dcb7011c97a12149cf1abdef8e6c12
mdriadali <riayedislamrishan@gmail.com>
2026-06-20 02:07:26 +0600

    feat (http-server): add generationJobRouter and create route

---
+++ apps/http-server/src/presentation/routes/generationJob.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { Auth } from "../container/authMiddeleware-dependencies";
+import { generationJobController } from "../container/generationJobController-dependencies";
+
+const generationJobRouter=Router()
+
+generationJobRouter.post("/create", Auth,generationJobController.create)
+
+export default generationJobRouter
