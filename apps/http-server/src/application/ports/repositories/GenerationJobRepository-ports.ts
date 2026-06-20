f5c82d4b1d89a6de50b743316a223059eeec82cd
mdriadali <riayedislamrishan@gmail.com>
2026-06-21 01:20:26 +0600

    feat (http-server):  add findByidAndworkspaceMember in IGenerationJobRepository

--- apps/http-server/src/application/ports/repositories/GenerationJobRepository-ports.ts
+++ apps/http-server/src/application/ports/repositories/GenerationJobRepository-ports.ts
@@ -2,4 +2,5 @@ import type { GenerationJobData } from "@repo/types";
 
 export interface IGenerationJobRepository {
     create(workspaceId: string): Promise<GenerationJobData>
+    findByidAndworkspaceMember(userId:string,generationJobId:string):Promise<GenerationJobData|null>
 }
