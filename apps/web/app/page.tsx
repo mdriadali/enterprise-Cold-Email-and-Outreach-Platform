22f27890073bfb0ca29412a054b883e92cfeeb74
mdriadali <riayedislamrishan@gmail.com>
2026-06-27 01:20:49 +0600

    fix  (web):  remove  db call in page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,9 @@
 export const dynamic = "force-dynamic";
-import {prismaClient} from '@repo/db'
-import { sharedEnv } from '@repo/env/shared-env';
 
 export default async function Home() {
-  console.log("WEB DATABASE_URL =", sharedEnv.DATABASE_URL);
-  const users=await prismaClient.user.findMany()
   return (
     <div>
-      {JSON.stringify(users)||"hellow"}
+      {"hellow"}
     </div>
   );
 }
