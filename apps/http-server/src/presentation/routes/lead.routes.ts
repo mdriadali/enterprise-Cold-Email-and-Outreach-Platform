a6861c189b44ee5afd7ee432a1e2beb9779cf31a
mdriadali <riayedislamrishan@gmail.com>
2026-06-27 00:45:22 +0600

    feat (http-server): add  bulk-create route in leadRouter

--- apps/http-server/src/presentation/routes/lead.routes.ts
+++ apps/http-server/src/presentation/routes/lead.routes.ts
@@ -5,4 +5,5 @@ import { leadController } from "../container/leadController-dependencie";
 const leadRouter=Router()
 
 leadRouter.post("/create",Auth, leadController.create)
+leadRouter.post("/bulk-create",Auth,leadController.bulkCreate)
 export default leadRouter
