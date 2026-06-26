bc62948ba25842f10bd9bbda83b0ea52a2a0dad7
mdriadali <riayedislamrishan@gmail.com>
2026-06-27 01:13:47 +0600

    fix  (http-server): CreateBulkLeadUseCase  import ports in @repo/ports

---
+++ apps/http-server/src/application/use-cases/lead/createBulkLeadUseCase.ts
@@ -0,0 +1,57 @@
+import type { leadInputdata } from "@repo/types";
+
+import { LeadValidator } from "../../../domain/lead/leadvalidator";
+
+import type { IGenerationJobRepository, ILeadRepository } from "@repo/ports";
+
+export class CreateBulkLeadUseCase {
+
+    constructor(
+        private readonly generationJobRepository: IGenerationJobRepository,
+        private readonly leadRepository: ILeadRepository
+    ) { }
+
+    async execute(
+        userId: string,
+        generationJobId: string,
+        leads: leadInputdata[]
+    ) {
+
+
+        if (!Array.isArray(leads)) {
+            throw new Error(
+                "Leads must be an array"
+            );
+        }
+
+        if (leads.length === 0) {
+            throw new Error(
+                "No leads provided"
+            );
+        }
+
+        // Validate job access
+        const job =
+            await this.generationJobRepository
+                .findByidAndworkspaceMember(
+                    userId,
+                    generationJobId
+                );
+
+        LeadValidator.validateJobAcess(job);
+
+        // Validate every lead
+        for (const lead of leads) {
+            LeadValidator.validateInputData(
+                generationJobId,
+                lead
+            );
+        }
+
+        // Bulk insert
+        return this.leadRepository.createMany(
+            generationJobId,
+            leads
+        );
+    }
+}
