f9109153856859840ca891df777b9c85deded9cc
mdriadali <riayedislamrishan@gmail.com>
2026-06-27 01:00:56 +0600

    fix  (http-server): RefreshUseCase  import ports in @repo/ports and format style

--- apps/http-server/src/application/use-cases/auth/Refresh-UseCase.ts
+++ apps/http-server/src/application/use-cases/auth/Refresh-UseCase.ts
@@ -1,26 +1,26 @@
+import type { IJwtTokenProvider, IRefreshTokenRepository } from "@repo/ports";
 import { AuthValidator } from "../../../domain/auth/AuthValidator";
-import type { IJwtTokenProvider } from "../../ports/auth/IJwtTokenProvider-ports";
-import type { IRefreshTokenRepository } from "../../ports/repositories/RefreshTokenRepository-ports";
 
-export class RefreshUseCase{
+
+export class RefreshUseCase {
 
     constructor(
-        private readonly refreshTokenRepository:IRefreshTokenRepository,
-        private readonly jwtTokenProvider :IJwtTokenProvider
-    ){}
+        private readonly refreshTokenRepository: IRefreshTokenRepository,
+        private readonly jwtTokenProvider: IJwtTokenProvider
+    ) { }
 
-    async execute(token:string){
+    async execute(token: string) {
         AuthValidator.tokenValidator(token)
 
         await this.jwtTokenProvider.validateRefreshToken(token)
 
-       const tokenData= await this.refreshTokenRepository.findByToken(token)
+        const tokenData = await this.refreshTokenRepository.findByToken(token)
 
-       AuthValidator.tokenDataValidator(tokenData)
+        AuthValidator.tokenDataValidator(tokenData)
 
-       AuthValidator.refreshTokenExpire(tokenData!.expiresAt)
+        AuthValidator.refreshTokenExpire(tokenData!.expiresAt)
 
-       const accessToken =await this.jwtTokenProvider.generateAccessToken(tokenData!.userId)
-       return {accessToken}
+        const accessToken = await this.jwtTokenProvider.generateAccessToken(tokenData!.userId)
+        return { accessToken }
     }
 }
