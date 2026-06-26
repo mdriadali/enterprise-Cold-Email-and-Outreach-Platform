e9d1edaa095568f4d062c5281e8c2bd1c83e16e8
mdriadali <riayedislamrishan@gmail.com>
2026-06-27 01:13:09 +0600

    fix  (http-server): CreateAiAPiUseCase  import ports in @repo/ports

--- apps/http-server/src/application/use-cases/aiApi/createAiApi-useCase.ts
+++ apps/http-server/src/application/use-cases/aiApi/createAiApi-useCase.ts
@@ -1,14 +1,15 @@
 import type { AiProvider } from "@repo/db";
 import { AiApiValidator } from "../../../domain/aiApi/aiapivalidator";
-import type { IAiApiRepository } from "../../ports/repositories/AiApiRepository-ports";
+import type { IAiApiRepository } from "@repo/ports";
+
 
 export class CreateAiAPiUseCase {
     constructor(
-        private readonly aiApiRepository:IAiApiRepository
+        private readonly aiApiRepository: IAiApiRepository
     ) { }
-    async execute(userId:string,provider: AiProvider, apiKey: string) {
+    async execute(userId: string, provider: AiProvider, apiKey: string) {
         AiApiValidator.createInput(provider, apiKey)
-        const newApi=await this.aiApiRepository.create(userId,provider,apiKey)
+        const newApi = await this.aiApiRepository.create(userId, provider, apiKey)
         return newApi
     }
 }
