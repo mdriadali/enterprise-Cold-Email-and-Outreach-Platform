30d687daa8cfce8074fd23a7014839a3f7239cad
mdriadali <riayedislamrishan@gmail.com>
2026-06-26 16:21:10 +0600

    feat (env): add schema file

---
+++ packages/env/src/schema.ts
@@ -0,0 +1,20 @@
+
+import z from "zod";
+
+export const databaseSchema = {
+  DATABASE_URL: z.string().url(),
+};
+
+export const authSchema = {
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_REFRESH_SECRET: z.string(),
+};
+
+export const httpSchema = {
+  HTTP_PORT: z.string(),
+};
+
+export const redisSchema = {
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.coerce.number(),
+};
