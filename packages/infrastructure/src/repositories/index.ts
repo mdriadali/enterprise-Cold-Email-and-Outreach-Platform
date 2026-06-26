af78014975f7854b714c0525c1199dee43f949af
mdriadali <riayedislamrishan@gmail.com>
2026-06-26 18:01:14 +0600

    feat (infrastructure): repository index for all repository export

---
+++ packages/infrastructure/src/repositories/index.ts
@@ -0,0 +1,7 @@
+export { PrismaAiApiRepository } from "./PrismaAiApiRepository";
+export { PrismaGenerationJobRepository } from "./PrismaGenerationJobRepository";
+export { PrismaLeadRepository } from "./PrismaLeadRepository";
+export { PrismaRefreshToken } from "./PrismaRefreshToken";
+export { PrismaUserRepository } from "./PrismaUserRepository";
+export { PrismaWorkspace } from "./PrismaWorkspace";
+export { PrismaWorkspaceMember } from "./PrismaWorkspaceMemberRepository";
