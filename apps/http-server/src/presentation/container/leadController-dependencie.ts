8f6cef38a23bd631293ebd0fb7d862ea071595bd
mdriadali <riayedislamrishan@gmail.com>
2026-06-27 00:44:38 +0600

    feat (http-server): add createBulkLeadUseCase in  leadController dependencie

--- apps/http-server/src/presentation/container/leadController-dependencie.ts
+++ apps/http-server/src/presentation/container/leadController-dependencie.ts
@@ -1,3 +1,4 @@
+import { CreateBulkLeadUseCase } from "../../application/use-cases/lead/createBulkLeadUseCase";
 import { CreateLeadUseCase } from "../../application/use-cases/lead/createLead-useCase";
 import { LeadController } from "../controllers/LeadController";
 import { generationJobRepository, leadRepository } from "./share-dependencies";
@@ -6,4 +7,10 @@ const createLeadUseCase=new CreateLeadUseCase(
     generationJobRepository,
     leadRepository
 )
-export const leadController= new LeadController(createLeadUseCase)
+
+const createBulkLeadUseCase =new CreateBulkLeadUseCase(
+    generationJobRepository,
+    leadRepository
+)
+
+export const leadController= new LeadController(createLeadUseCase,createBulkLeadUseCase)
