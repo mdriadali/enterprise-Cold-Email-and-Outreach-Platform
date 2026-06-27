060448e409968647702f68f4a3807ac734581e40
mdriadali <riayedislamrishan@gmail.com>
2026-06-28 02:05:31 +0600

    fix (worker): redis connection string  importand style format in quota reset worker

--- apps/worker/src/workers/quotaReset.worker.ts
+++ apps/worker/src/workers/quotaReset.worker.ts
@@ -1,24 +1,28 @@
 import { Worker } from "bullmq";
 import { aiApiRepository } from "../container/dependencies";
+import { queueConnection } from "@repo/queue";
 
 new Worker(
     "reset-api-key",
 
-    async (RATE_LIMITED) => {
+    async (job) => {
         console.log("[Quota Reset Worker] Job Received");
 
 
-        const { apiId } = RATE_LIMITED.data as {
-            apiId: string;
+        const { apiKeyId } = job.data as {
+            apiKeyId: string;
         }
 
         try {
-            const updateStatus = await aiApiRepository.updateStatus(apiId, "AVAILABLE")
-            console.log(`[Quota Reset Worker] ApiId ${apiId} Status Update Sucessfully `)
+            const resetApiUsage = await aiApiRepository.updateStatus(apiKeyId,"AVAILABLE")
+            console.log("Quota reset worker status:", resetApiUsage);
         } catch (error) {
-            console.log(`[Quota Reset Worker] ApiId ${apiId} Status Update Unsucessfully `)
             console.log("Qota Reset Worker Internal error:", error)
         }
 
+    },
+    {
+        connection: queueConnection as any,
+        concurrency: 5,
     }
 )
