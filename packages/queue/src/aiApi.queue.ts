5b9896a1e52f7c5a42a5b4c616bf6bed495e4f93
mdriadali <riayedislamrishan@gmail.com>
2026-06-29 01:22:27 +0600

    fix (queue): change variable name quotaResetQueue  to resetApiStatusQueue and queue name   reset-api-key to reset-api-key-status in aiApi queue

--- packages/queue/src/aiApi.queue.ts
+++ packages/queue/src/aiApi.queue.ts
@@ -1,8 +1,8 @@
 import { Queue } from "bullmq";
 import { queueConnection } from "./connection";
 
-export const quotaResetQueue = new Queue(
-  "reset-api-key",
+export const resetApiStatusQueue = new Queue(
+  "reset-api-key-status",
   {
     connection: queueConnection as any,
   }
