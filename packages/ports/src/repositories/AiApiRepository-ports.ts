77449947f9b3a13dc9869d4f336cd6791eebf3a1
mdriadali <riayedislamrishan@gmail.com>
2026-06-29 01:17:36 +0600

    feat (ports): add getApiSummary in IAiApiRepository

--- packages/ports/src/repositories/AiApiRepository-ports.ts
+++ packages/ports/src/repositories/AiApiRepository-ports.ts
@@ -1,9 +1,10 @@
 import type { aiApiStatus, AiProvider } from "@repo/db";
-import type { aiApiData } from "@repo/types";
+import type { aiApiData, ApiSummary } from "@repo/types";
 
 export interface IAiApiRepository{
     create(ownerId:string,provider:AiProvider,key:string):Promise<aiApiData>
     findByOwnerId(ownerId:string):Promise<aiApiData[]>
-    updateStatus(id:string , status:aiApiStatus):Promise<void>
+    getApiSummary(ownerId:string):Promise<ApiSummary>;
+    updateStatus(id:string , status:aiApiStatus):Promise<aiApiData>
     findAvailableByOwnerId(ownerId:string):Promise<aiApiData[]>
 }
