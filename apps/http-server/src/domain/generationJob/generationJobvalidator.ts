2b018d0d82384b21a72fde0a11d1df225c3834e8
mdriadali <riayedislamrishan@gmail.com>
2026-06-29 19:31:18 +0600

    feat (http-server): add isGenerationJobExist remove validateUserAcessJob and validateGenerationId error change and validatememberdata add error massage in GenerationJobValidator

--- apps/http-server/src/domain/generationJob/generationJobvalidator.ts
+++ apps/http-server/src/domain/generationJob/generationJobvalidator.ts
@@ -1,6 +1,7 @@
 import type { GenerationJobData, workspaceMemberData } from "@repo/types";
-import { GenerationIdInvalidError, WorkspaceIdInvalidError } from "./generationJobError";
+import { GenerationError, WorkspaceIdInvalidError } from "./generationJobError";
 import { notAccess } from "../sharedError";
+import { GenerationJobStatus } from "@repo/db";
 
 export class GenerationJobValidator {
 
@@ -15,19 +16,30 @@ export class GenerationJobValidator {
 
   static validatememberdata(data: workspaceMemberData | null) {
     if (!data) {
-      throw new notAccess()
+      throw new notAccess("Workspace Or This Member Not Exist ")
     }
   }
 
   static validateGenerationId(id: string | null) {
     if (!id) {
-      throw new GenerationIdInvalidError()
+      throw new GenerationError("Generation Id Invalid")
     }
   }
 
-  static validateUserAcessJob(data: GenerationJobData |null) {
-    if (!data) {
-      throw new notAccess()
+
+  static isGenerationJobExist(data:GenerationJobData|null){
+    if(!data){
+      throw new GenerationError("This Email Generation Job Not Exist")
+    }
+  }
+
+  static jobCanStart(status:GenerationJobStatus |null){
+    if(status==="PROCESSING"){
+      console.log("This Email Genaration Job Already Processing")
+      throw new GenerationError("This Email Genaration Job Already Processing")
+    }
+    if(status==="COMPLETED"){
+      throw new GenerationError("This Email Genaration Job Already Completed")
     }
   }
 }
