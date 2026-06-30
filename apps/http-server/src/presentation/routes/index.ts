34bc6f16696191574c28eddb93796385bcecfdd0
mdriadali <riayedislamrishan@gmail.com>
2026-06-30 19:54:29 +0600

    fix (http-server): remove aiapi, generationjob,lead route in  router

--- apps/http-server/src/presentation/routes/index.ts
+++ apps/http-server/src/presentation/routes/index.ts
@@ -2,17 +2,12 @@ import { Router } from "express";
 import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
 import workspaceRouter from "./workspace.routes";
-import aiApiRouter from "./aiapi.routes";
-import generationJobRouter from "./generationJob.routes";
-import leadRouter from "./lead.routes";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/user",userRouter);
 router.use("/workspace",workspaceRouter)
-router.use("/aiapi",aiApiRouter)
-router.use("/generationjob",generationJobRouter)
-router.use("/lead",leadRouter)
+
 
 export default router;
