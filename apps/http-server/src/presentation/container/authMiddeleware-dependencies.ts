c8e2e55971823eef9438777041eed321df7b3cc8
mdriadali <riayedislamrishan@gmail.com>
2026-06-30 18:46:31 +0600

    refactor (http-server): move code another file

--- apps/http-server/src/presentation/container/authMiddeleware-dependencies.ts
+++
@@ -1,9 +0,0 @@
-import { AuthMiddleware } from "../middlewares/AuthMiddleware";
-import { jwtTokenGenerator, prismaUserRepository } from "./share-dependencies";
-
-const authMiddleware = new AuthMiddleware(
-    jwtTokenGenerator,
-    prismaUserRepository,
-)
-
-export const Auth = authMiddleware.execute.bind(authMiddleware)
