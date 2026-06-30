0b0380ca5dc43ea8c510270db50769073462f7fb
mdriadali <riayedislamrishan@gmail.com>
2026-06-30 18:37:39 +0600

    feat (http-server): WorkspaceMiddleware

---
+++ apps/http-server/src/presentation/middlewares/WorkspaceMiddleware.ts
@@ -0,0 +1,44 @@
+import type { IWorkspaceMemberRepository } from "@repo/ports";
+import type { NextFunction, Request, Response } from "express";
+import { WorkspaceValidator } from "../../domain/workspace/workspaceValidator";
+import { AppError } from "../../domain/AppError";
+
+export interface WorkspaceParams {
+    workspaceId: string;
+}
+
+export class WorkspaceMiddleware {
+    constructor(
+        private readonly workspaceMemberRepository: IWorkspaceMemberRepository
+    ) { }
+    async execute(req: Request, res: Response, next: NextFunction) {
+        console.log("[Workspace Middleware] Request Recived")
+        const userId = req.user.id
+        const { workspaceId } = req.params as unknown as WorkspaceParams
+        try {
+            WorkspaceValidator.validateId(workspaceId ?? null)
+            const findWorkspaceMember = await this.workspaceMemberRepository.findByWorkspaceAndUser(workspaceId, userId)
+            WorkspaceValidator.validateMemberdata(findWorkspaceMember)
+            req.workspaceMember = {
+                id: findWorkspaceMember!.id,
+                workspaceId: findWorkspaceMember!.workspaceId,
+                userId: findWorkspaceMember!.userId,
+                role: findWorkspaceMember!.role,
+                createdAt: findWorkspaceMember!.createdAt
+            }
+            console.log("[Workspace Middleware] user Validate sucessfully")
+            return next()
+        } catch (error) {
+            if (error instanceof AppError) {
+                return res.status(400).json({
+                    messae: error.message
+                })
+            }
+            console.log("[Workspace Middleware] Internal Server Error ", error)
+            return res.status(500).json({
+                messae: " Internal Server Error"
+            })
+        }
+
+    }
+}
