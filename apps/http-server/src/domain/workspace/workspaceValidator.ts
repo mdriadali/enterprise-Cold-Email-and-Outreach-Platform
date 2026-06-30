d519ff6e802935759b729e57725302d948f22a3d
mdriadali <riayedislamrishan@gmail.com>
2026-06-30 18:35:48 +0600

    feat (http-server): add validateId , validateMemberdata and change WorkspaceValidator name

--- apps/http-server/src/domain/workspace/workspaceValidator.ts
+++ apps/http-server/src/domain/workspace/workspaceValidator.ts
@@ -1,7 +1,8 @@
-import { workspaceNameInvlid, workspaceNameMaxError, workspaceNameMinError } from "./workspaceError"
+import type { workspaceMemberData } from "@repo/types"
+import { WorkspaceError, workspaceNameInvlid, workspaceNameMaxError, workspaceNameMinError } from "./workspaceError"
 import { workspacerules } from "./workspaceRules"
 
-export class workspaceValidator {
+export class WorkspaceValidator {
     static validateName(name: string) {
         if (!name) {
             throw new workspaceNameInvlid()
@@ -9,8 +10,19 @@ export class workspaceValidator {
         if (name.length < workspacerules.MIN_NAME) {
             throw new workspaceNameMinError()
         }
-        if(name.length>workspacerules.MAX_NAME){
+        if (name.length > workspacerules.MAX_NAME) {
             throw new workspaceNameMaxError()
+        }
+    }
+    static validateId(id: string | null) {
+        if (!id) {
+            throw new WorkspaceError("WorkspaceId Invalid")
+        }
+    }
+
+    static validateMemberdata(data:workspaceMemberData |null){
+        if(!data){
+            throw new WorkspaceError("This user is not a member of this workspace.")
         }
     }
 }
