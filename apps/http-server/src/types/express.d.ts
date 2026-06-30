4e4160dcf196978d71f3bbdd52632306af6065b2
mdriadali <riayedislamrishan@gmail.com>
2026-06-30 18:49:29 +0600

    feat (http-server): add workspaceMember in global types

--- apps/http-server/src/types/express.d.ts
+++ apps/http-server/src/types/express.d.ts
@@ -1,9 +1,11 @@
+import type { workspaceMemberData } from "@repo/types";
 import type { User } from "../domain/user/User";
 
 declare global {
   namespace Express {
     interface Request {
       user?: User;
+       workspaceMember?: workspaceMemberData
     }
   }
 }
