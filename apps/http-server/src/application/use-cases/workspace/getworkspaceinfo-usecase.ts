84d16ed550e8d89ace84041642a1b962f2e7a486
mdriadali <riayedislamrishan@gmail.com>
2026-07-01 20:49:37 +0600

    feat (http-server): GetWorkspaceInfoUseCase

---
+++ apps/http-server/src/application/use-cases/workspace/getworkspaceinfo-usecase.ts
@@ -0,0 +1,17 @@
+import type { IWorkspaceRepository } from "@repo/ports";
+import { WorkspaceValidator } from "../../../domain/workspace/workspaceValidator";
+import { PlanService } from "../../../domain/subscription/PlanService";
+
+export class GetWorkspaceInfoUseCase {
+    constructor(
+        private readonly workspaceRepository: IWorkspaceRepository
+    ) { }
+    async execute(workspaceid: string) {
+        WorkspaceValidator.validateId(workspaceid)
+        const info = await this.workspaceRepository.info(workspaceid)
+        WorkspaceValidator.validateInfoData(info)
+        const owner = info?.members.find(member => member.role === "OWNER")
+      const limits=  PlanService.getLimits(owner!.user.subscription)
+        return {limits,info}
+    }
+}
