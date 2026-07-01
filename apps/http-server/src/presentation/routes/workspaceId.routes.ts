cc5bf0e6a929e29ced89b2e99ae773fd7f8aacf9
mdriadali <riayedislamrishan@gmail.com>
2026-07-01 20:50:52 +0600

    feat (http-server): add info route in workspaceIdRouter

--- apps/http-server/src/presentation/routes/workspaceId.routes.ts
+++ apps/http-server/src/presentation/routes/workspaceId.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import aiApiRouter from "./aiapi.routes";
 import generationJobRouter from "./generationJob.routes";
-import leadRouter from "./lead.routes";
 import smtpRouter from "./smtp.routes";
+import { workspaceController } from "../container/workspacecontroller-dependencies";
 
 const workspaceIdRouter = Router()
+workspaceIdRouter.get("/info",workspaceController.info)
+
 workspaceIdRouter.use("/aiapi", aiApiRouter)
 workspaceIdRouter.use("/generationjob", generationJobRouter)
 workspaceIdRouter.use("/smtp", smtpRouter)
