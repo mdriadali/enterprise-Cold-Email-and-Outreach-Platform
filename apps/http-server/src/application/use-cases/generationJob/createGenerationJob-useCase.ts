ff86b3215bed5774fa4d4f2b35488ceae49831f7
mdriadali <riayedislamrishan@gmail.com>
2026-07-01 20:24:26 +0600

    fix (worker): add name  value in  newjob  in  CreategenerationJobUseCase

--- apps/http-server/src/application/use-cases/generationJob/createGenerationJob-useCase.ts
+++ apps/http-server/src/application/use-cases/generationJob/createGenerationJob-useCase.ts
@@ -7,11 +7,11 @@ export class CreategenerationJobUseCase {
         private readonly workspaceMemberRepository: IWorkspaceMemberRepository,
         private readonly generationJobRepository: IGenerationJobRepository
     ) { }
-    async execute(userId: string, workspaceId: string) {
+    async execute(userId: string, workspaceId: string, name:string) {
         GenerationJobValidator.validateCreateData(workspaceId)
         const findWorkspaceMember = await this.workspaceMemberRepository.findByWorkspaceAndUser(workspaceId, userId)
         GenerationJobValidator.validatememberdata(findWorkspaceMember)
-        const newjob = await this.generationJobRepository.create(workspaceId)
+        const newjob = await this.generationJobRepository.create(workspaceId,name)
         return newjob
     }
 }
