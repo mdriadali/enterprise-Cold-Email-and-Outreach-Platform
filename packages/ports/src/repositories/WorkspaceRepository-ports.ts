43a3b6c939ce12fe82e804a211b033f108b1e7b6
mdriadali <riayedislamrishan@gmail.com>
2026-07-01 20:08:02 +0600

    feat (ports): add info in IWorkspaceRepository

--- packages/ports/src/repositories/WorkspaceRepository-ports.ts
+++ packages/ports/src/repositories/WorkspaceRepository-ports.ts
@@ -1,6 +1,7 @@
-import type { workspaceData } from "@repo/types";
+import type { workspaceData, workspaceInfoData } from "@repo/types";
 
 export interface IWorkspaceRepository{
     create(userId:string,name:string):Promise<workspaceData>,
     findById(id:string):Promise<workspaceData |null>
+    info(workspaceId:string):Promise<workspaceInfoData|null>
 }
