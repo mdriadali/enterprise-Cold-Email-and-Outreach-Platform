efaa5cd30e1052dad1ab2ce74f8c15fc75ffd250
mdriadali <riayedislamrishan@gmail.com>
2026-07-01 20:17:57 +0600

    fix (ports): findbyOwnerId to findByWorkspaceId and chane args  and getApiSummary args and findAvailableByOwnerId to findAvailableByWorkspaceId and chane args in IAiApiRepository

--- packages/ports/src/repositories/AiApiRepository-ports.ts
+++ packages/ports/src/repositories/AiApiRepository-ports.ts
@@ -3,8 +3,8 @@ import type { aiApiData, ApiSummary } from "@repo/types";
 
 export interface IAiApiRepository{
     create(ownerId:string,workspaceid:string,provider:AiProvider,key:string):Promise<aiApiData>
-    findByOwnerId(ownerId:string):Promise<aiApiData[]>
-    getApiSummary(ownerId:string):Promise<ApiSummary>;
+    findByWorkspaceId(workspaceId:string):Promise<aiApiData[]>
+    getApiSummary(workspaceId:string):Promise<ApiSummary>;
     updateStatus(id:string , status:aiApiStatus):Promise<aiApiData>
-    findAvailableByOwnerId(ownerId:string):Promise<aiApiData[]>
+    findAvailableByWorkspaceId(workspaceId:string):Promise<aiApiData[]>
 }
