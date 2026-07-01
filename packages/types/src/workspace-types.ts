b7e73eeba81fc36e2d30014dff246ccae07ac72a
mdriadali <riayedislamrishan@gmail.com>
2026-07-01 20:07:21 +0600

    feat (types): add workspaceInfoData in workspace types

--- packages/types/src/workspace-types.ts
+++ packages/types/src/workspace-types.ts
@@ -1,4 +1,6 @@
-import type { WorkspaceMemberRole } from "@repo/db"
+import type { aiApiStatus, AiProvider, GenerationJobStatus, WorkspaceMemberRole } from "@repo/db"
+import type { GenerationJobData } from "./generationJob-types"
+import type { aiApiData } from "./aiApi-types"
 
 export interface workspaceData {
     id: string,
@@ -15,7 +17,52 @@ export interface workspaceMemberCreateData {
 export interface workspaceMemberData {
     id: string,
     workspaceId: string,
-    userId:string,
-    role:WorkspaceMemberRole,
-    createdAt:Date
+    userId: string,
+    role: WorkspaceMemberRole,
+    createdAt: Date
+}
+
+
+
+
+export interface workspaceInfoData {
+  id: string;
+  name: string;
+
+  _count: {
+    members: number;
+    generationJob: number;
+    AiApiKeys: number;
+  };
+
+  members: {
+    id: string;
+    role: WorkspaceMemberRole;
+
+    user: {
+      id: string;
+      name: string;
+      email: string;
+    };
+  }[];
+
+  generationJob: {
+    id: string;
+    name: string;
+    status: GenerationJobStatus;
+
+    totalLeads: number |null;
+    successCount: number | null;
+    failedCount: number | null;
+    pendingCount: number | null;
+
+    createdAt: Date;
+  }[];
+
+  AiApiKeys: {
+    id: string;
+    aiProvider: AiProvider;
+    status: aiApiStatus;
+
+  }[];
 }
