5dc2830cdfa5838ae132a23cf83ced7323813693
mdriadali <riayedislamrishan@gmail.com>
2026-07-02 18:15:53 +0600

    feat (http-server): add  get  method in GenerationJobController

--- apps/http-server/src/presentation/controllers/generationJobController.ts
+++ apps/http-server/src/presentation/controllers/generationJobController.ts
@@ -2,19 +2,21 @@ import type { Request, Response } from "express";
 import { AppError } from "../../domain/AppError";
 import type { CreategenerationJobUseCase } from "../../application/use-cases/generationJob/createGenerationJob-useCase";
 import type { StartGenerationJobUseCase } from "../../application/use-cases/generationJob/startGenerationJob-usecase";
+import type { GetGenerationJobUseCase } from "../../application/use-cases/generationJob/getGenerationJob-useCase";
 
 export class GenerationJobController {
     constructor(
         private readonly creategenerationJobUseCase: CreategenerationJobUseCase,
-        private readonly startGenerationJobUseCase: StartGenerationJobUseCase
+        private readonly startGenerationJobUseCase: StartGenerationJobUseCase,
+        private readonly getGenerationJobUseCase: GetGenerationJobUseCase
     ) { }
     create = async (req: Request, res: Response) => {
         try {
             console.log("[GenerationJob Create] Request Recived ")
             const userid = req.user.id
-            const  workspaceId  = req.workspaceMember!.workspaceId
-            const {name}=req.body
-            const generationJob = await this.creategenerationJobUseCase.execute(userid, workspaceId,name)
+            const workspaceId = req.workspaceMember!.workspaceId
+            const { name } = req.body
+            const generationJob = await this.creategenerationJobUseCase.execute(userid, workspaceId, name)
             console.log("[GenerationJob Create] Sucessfully")
             return res.status(200).json({
                 generationJob
@@ -32,18 +34,39 @@ export class GenerationJobController {
             })
         }
     }
+    get = async (req: Request, res: Response) => {
+        try {
+            console.log("[GenerationJob Get] Request Recived")
+            const generatioJobId = req.params.generationJobId
+            const workspaceId = req.workspaceMember?.workspaceId
+            const job = await this.getGenerationJobUseCase.execute(workspaceId as string, generatioJobId as string)
+            console.log("[GenerationJob Get] Data Send Sucessfully")
+            return res.status(200).json(job)
+        } catch (error) {
+            if (error instanceof AppError) {
+                return res.status(400).json({
+                    message: error.message
+                })
+            }
+
+            console.log("[GenerationJob Get] Internal Server Error", error)
+            return res.status(500).json({
+                message: "Internal Server Error"
+            })
+        }
+    }
     start = async (req: Request, res: Response) => {
         try {
             console.log("[GenerationJob Start] Request Recived")
 
             const userId = req.user.id
-           const { jobid } = req.params;
+            const { jobid } = req.params;
             const start = await this.startGenerationJobUseCase.execute(userId, jobid as string)
 
             console.log("[GenerationJob Start] Sucessfully JobId:", jobid)
 
             return res.status(200)
-            .json({ sucess: true, message: "Genaration Start JobId:", jobid })
+                .json({ sucess: true, message: "Genaration Start JobId:", jobid })
 
         } catch (error) {
             if (error instanceof AppError) {
