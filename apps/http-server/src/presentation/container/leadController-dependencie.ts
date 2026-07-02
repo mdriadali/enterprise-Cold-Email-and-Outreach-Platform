0ce81a09b87e4fe124d7015beda7b21085022c88
mdriadali <riayedislamrishan@gmail.com>
2026-07-03 00:50:24 +0600

    feat (http-server): add allLeadFindUseCase in leadController dependencie

--- apps/http-server/src/presentation/container/leadController-dependencie.ts
+++ apps/http-server/src/presentation/container/leadController-dependencie.ts
@@ -1,3 +1,4 @@
+import { AllLeadFindUseCase } from "../../application/use-cases/lead/allLeadFind-UseCase";
 import { CreateBulkLeadUseCase } from "../../application/use-cases/lead/createBulkLeadUseCase";
 import { CreateLeadUseCase } from "../../application/use-cases/lead/createLead-useCase";
 import { LeadController } from "../controllers/LeadController";
@@ -12,5 +13,8 @@ const createBulkLeadUseCase =new CreateBulkLeadUseCase(
     generationJobRepository,
     leadRepository
 )
+const allLeadFindUseCase=new AllLeadFindUseCase(
+    leadRepository
+)
 
-export const leadController= new LeadController(createLeadUseCase,createBulkLeadUseCase)
+export const leadController= new LeadController(createLeadUseCase,createBulkLeadUseCase,allLeadFindUseCase)
