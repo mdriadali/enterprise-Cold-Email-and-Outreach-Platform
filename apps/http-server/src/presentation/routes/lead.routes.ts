f5e0a6887b9ddfbb35bfb8505600856c268f05d8
mdriadali <riayedislamrishan@gmail.com>
2026-07-03 00:50:51 +0600

    feat (http-server): add all leads find route in leadRouter

--- apps/http-server/src/presentation/routes/lead.routes.ts
+++ apps/http-server/src/presentation/routes/lead.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { Auth } from "../container/Middeleware-dependencies";
 import { leadController } from "../container/leadController-dependencie";
 
 const leadRouter = Router({ mergeParams: true })
 
 leadRouter.post("/create", leadController.create)
 leadRouter.post("/bulk-create",leadController.bulkCreate)
+leadRouter.get("/all", leadController.allLeadFind)
 export default leadRouter
