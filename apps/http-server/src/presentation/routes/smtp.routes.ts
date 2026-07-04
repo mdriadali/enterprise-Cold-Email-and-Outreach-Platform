b76797f75b868d0a2d7fbf77a42edd86d9ddf57e
mdriadali <riayedislamrishan@gmail.com>
2026-07-04 23:47:30 +0600

    feat(http-server): add all  smtp acconts find route in smtpRouter

--- apps/http-server/src/presentation/routes/smtp.routes.ts
+++ apps/http-server/src/presentation/routes/smtp.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { smtpAccountController } from "../container/smtpAccountController-dependencie";
 
-const smtpRouter=Router()
+const smtpRouter = Router()
 
-smtpRouter.post("/create",smtpAccountController.create)
+smtpRouter.post("/create", smtpAccountController.create)
+smtpRouter.get("/all", smtpAccountController.findAllAcounts)
 export default smtpRouter
