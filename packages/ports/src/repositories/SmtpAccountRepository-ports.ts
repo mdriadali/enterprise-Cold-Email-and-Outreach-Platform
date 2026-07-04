e70c97aa980d5d20e5fde09ad5c58f13f45cfba1
mdriadali <riayedislamrishan@gmail.com>
2026-07-04 23:42:13 +0600

    feat(ports):add findByIdAndWorspaceId and  findByWorkspaceId in ISmtpAccountRepository

--- packages/ports/src/repositories/SmtpAccountRepository-ports.ts
+++ packages/ports/src/repositories/SmtpAccountRepository-ports.ts
@@ -2,4 +2,6 @@ import type { SmtpCreateInputData, SmtpData } from "@repo/types";
 
 export interface ISmtpAccountRepository{
     create(data:SmtpCreateInputData):Promise<SmtpData>
+    findByIdAndWorspaceId(id:string, workspaceId:string):Promise<SmtpData|null>
+    findByWorkspaceId(workspaceId:string):Promise<SmtpData[]>
 }
