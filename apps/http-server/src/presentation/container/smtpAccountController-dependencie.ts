fc7010bbc7ed6bd0d14ae64e7c42196c18edf754
mdriadali <riayedislamrishan@gmail.com>
2026-07-04 23:46:52 +0600

    feat(http-server): add findAllSmtpAccountUseCase  in smtpAccountController dependencie

--- apps/http-server/src/presentation/container/smtpAccountController-dependencie.ts
+++ apps/http-server/src/presentation/container/smtpAccountController-dependencie.ts
@@ -1,8 +1,12 @@
 import { CreateSmtpAccountuseCase } from "../../application/use-cases/smtp/createSmtpAccount-usecase";
+import { FindAllSmtpAccountUseCase } from "../../application/use-cases/smtp/findAllSmtpAccount-useCase";
 import { SmtpAccountController } from "../controllers/smtpController";
 import { smtpAccountRepository } from "./share-dependencies";
 
 const createSmtpAccountuseCase=new CreateSmtpAccountuseCase(
     smtpAccountRepository
 )
-export const smtpAccountController=new SmtpAccountController(createSmtpAccountuseCase)
+const findAllSmtpAccountUseCase=new FindAllSmtpAccountUseCase(
+    smtpAccountRepository
+)
+export const smtpAccountController=new SmtpAccountController(createSmtpAccountuseCase,findAllSmtpAccountUseCase)
