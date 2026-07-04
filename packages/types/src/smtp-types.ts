0a945f8e4a096792efb729f1ec4ec7e76a40a66c
mdriadali <riayedislamrishan@gmail.com>
2026-07-04 18:47:10 +0600

    feat (types): add SmtpCreateInputData and SmtpData in smtp-types

---
+++ packages/types/src/smtp-types.ts
@@ -0,0 +1,42 @@
+import { SmtpEncryption } from "@repo/db"
+
+export interface SmtpCreateInputData {
+    workspaceId: string,
+    name: string,
+
+    host: string,
+    port: number,
+    username: string,
+    password: string,
+
+    fromName: string,
+    fromEmail: string,
+    replyTo?: string,
+
+    encryption: SmtpEncryption
+}
+export interface SmtpData {
+    id: string,
+
+    workspaceId: string,
+
+    name: string,
+
+    host: string,
+    port: number,
+
+    username: string,
+    password: string,
+
+    fromName: string,
+    fromEmail: string,
+    replyTo?: string | null,
+    encryption: SmtpEncryption,
+
+    error?: string | null
+    isActive: boolean
+
+    createdAt: Date
+
+
+}
